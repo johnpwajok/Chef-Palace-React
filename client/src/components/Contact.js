ef742bedcef0d20d0104a167a9646eb82e011d74
John Pwajok <johnpwajok97@hotmail.com>
2020-08-23 17:39:37 +0100

    Com removed and contact page updated

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,43 +1,44 @@
 import React, { Component } from "react";
 import "./styles.css";
+import FadeIn from "react-fade-in";
 
 export default class Contact extends Component {
   render() {
     return (
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6" className="mapDiv">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2379.7637378463774!2d-6.40249544843722!3d53.38327667988863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486772ae4d2921f3%3A0xe4debe1ff05a64c2!2sChef+Thai+%26+Chinese+Takeaway!5e0!3m2!1sen!2sie!4v1510599710533"
-              width="500"
-              height="450"
-              frameborder="0"
-              allowfullscreen
-            ></iframe>
-          </div>
-          <div class="col-md-6" className="aboutDiv">
+      <div class="container" className="contactContainer">
+        <FadeIn>
+          <div class="row" className="outerContact">
             <textheading> Our Locations!</textheading>
-
             <br></br>
+            <br></br>
+            <div class="col-md-6 col-sm-12" className="mapDiv">
+              <iframe
+                class="responsive-iframe"
+                src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d152285.06621937343!2d-6.498099450100997!3d53.3888153001904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1schef%20palace!5e0!3m2!1sen!2sie!4v1598199689458!5m2!1sen!2sie"
+              ></iframe>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12" className="aboutDiv">
+              <br></br>
 
-            <text>
-              <dl>
-                <dt>Unit 4, Coolmine Buisness Park, Clonsilla Road, D15</dt>
-                <dd>- 01-822 3818 / 822 3828</dd>
-                <br></br>
-                <dt>Unit 5, Black C, Tyrrelstown, Dublin 15</dt>
-                <dd>- 01-885 6011 / 827 4629</dd>
-                <br></br>
-                <dt>1 & 3 Eagle Court, Main Street, Clonee Village, D15</dt>
-                <dd>- 01-802 6478 / 802 6482</dd>
-                <br></br>
-                <dt>Unit 5, Sweeney Mews, Ongar Village, D15</dt>
-                <dd>- 01-826 1762 / 826 1763</dd>
-                <br></br>
-              </dl>
-            </text>
+              <text>
+                <dl>
+                  <dt>5 Sweeney Mews, Ongar Village, Dublin, D15</dt>
+                  <dd>- 01-822 3818 / 822 3828</dd>
+                  <br></br>
+                  <dt>5C Tyrrelstown Way, Cruiserath, Dublin, D15 E3C9</dt>
+                  <dd>- 01-885 6011 / 827 4629</dd>
+                  <br></br>
+                  <dt>1 & Main St, Ballymun, Dublin</dt>
+                  <dd>- 01-802 6478 / 802 6482</dd>
+                  <br></br>
+                  <dt>Main St, Portan, Clonee, Co. Meath</dt>
+                  <dd>- 01-826 1762 / 826 1763</dd>
+                  <br></br>
+                </dl>
+              </text>
+            </div>
           </div>
-        </div>
+        </FadeIn>
       </div>
     );
   }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./styles.css";
 import Carousel from "react-bootstrap/Carousel";
+import FadeIn from "react-fade-in";
 
 import logo from "../images/logo.png";
 import chef from "../images/chef.jpg";
@@ -15,101 +16,107 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <title>HomePage</title>
+        <FadeIn>
+          <title>HomePage</title>
 
-        <header>
-          <div class="container-fluid" id="container-head">
-            <center>
-              <img
-                src={logo}
-                alt="Chef Palace Logo"
-                id="mainImage"
-                class="img-responsive"
-              ></img>
-            </center>
-          </div>
-        </header>
-
-        <div class="container-fluid">
-          <section>
-            <text>
-              Hello and welcome to the Chef Palace Thai and Chinese Take Away
-              website!
-            </text>
-            <br></br>
-            <text>
-              <a href="Account.html"> Sign Up here </a> to see our full menu and
-              recieve exclusive offers & deals!
-            </text>
-            <br></br>
-            <br></br>
-          </section>
-        </div>
-        <div className="CarouselSection">
-          <Carousel>
-            <Carousel.Item interval={1000}>
-              <img className="d-block w-100" src={chef} alt="First slide" />
-            </Carousel.Item>
-            <Carousel.Item interval={1000}>
-              <img className="d-block w-100" src={chef2} alt="Third slide" />
-            </Carousel.Item>
-            <Carousel.Item interval={1000}>
-              <img className="d-block w-100" src={chef3} alt="Third slide" />
-            </Carousel.Item>
-          </Carousel>
-        </div>
-        <br></br>
-        <hr></hr>
-        <br></br>
-
-        <div class="container-fluid">
-          <center>
-            <textheading>Deals of the Week!</textheading>
-          </center>
-        </div>
-
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="jumbotron">
-                <img src={spicebag} alt="spicebag" class="img-responsive"></img>
-                <h2>Spice Bag</h2>
-                <text>
-                  Used to be : <pricecut>€7.99</pricecut>
-                  <br></br>
-                  Now : <pricechange>Only €4.99 !</pricechange>
-                </text>
-              </div>
-            </div>
-
-            <div class="col-md-6">
-              <div class="jumbotron">
+          <header>
+            <div class="container-fluid" id="container-head">
+              <center>
                 <img
-                  src={munchiebox}
-                  alt="munchiebox"
+                  src={logo}
+                  alt="Chef Palace Logo"
+                  id="mainImage"
                   class="img-responsive"
                 ></img>
-                <h2>Munchie Box</h2>
-                <text>
-                  Used to be : <pricecut> €11.99</pricecut>
-                  <br></br>
-                  Now : <pricechange> Only €7.99 !</pricechange>
-                </text>
-              </div>
+              </center>
             </div>
-            <div></div>
+          </header>
+
+          <div class="container-fluid">
+            <section>
+              <text>
+                Hello and welcome to the Chef Palace Thai and Chinese Take Away
+                website!
+              </text>
+              <br></br>
+              <text>
+                <a href="Account.html"> Sign Up here </a> to see our full menu
+                and recieve exclusive offers & deals!
+              </text>
+              <br></br>
+              <br></br>
+            </section>
+          </div>
+          <div className="CarouselSection">
+            <Carousel>
+              <Carousel.Item interval={1000}>
+                <img className="d-block w-100" src={chef} alt="First slide" />
+              </Carousel.Item>
+              <Carousel.Item interval={1000}>
+                <img className="d-block w-100" src={chef2} alt="Third slide" />
+              </Carousel.Item>
+              <Carousel.Item interval={1000}>
+                <img className="d-block w-100" src={chef3} alt="Third slide" />
+              </Carousel.Item>
+            </Carousel>
+            <br></br>
+            <hr></hr>
           </div>
           <br></br>
-          <center>
-            <text>
-              <a href="Account.html">Sign Up </a> To View Our Full Menu!
-            </text>
-          </center>
 
-          <br></br>
-          <hr></hr>
-          <br></br>
-        </div>
+          <div class="container-fluid">
+            <center>
+              <textheading>Deals of the Week!</textheading>
+            </center>
+          </div>
+
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="jumbotron">
+                  <img
+                    src={spicebag}
+                    alt="spicebag"
+                    class="img-responsive"
+                  ></img>
+                  <h2>Spice Bag</h2>
+                  <text>
+                    Used to be : <pricecut>€7.99</pricecut>
+                    <br></br>
+                    Now : <pricechange>Only €4.99 !</pricechange>
+                  </text>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="jumbotron">
+                  <img
+                    src={munchiebox}
+                    alt="munchiebox"
+                    class="img-responsive"
+                  ></img>
+                  <h2>Munchie Box</h2>
+                  <text>
+                    Used to be : <pricecut> €11.99</pricecut>
+                    <br></br>
+                    Now : <pricechange> Only €7.99 !</pricechange>
+                  </text>
+                </div>
+              </div>
+              <div></div>
+            </div>
+            <br></br>
+            <center>
+              <text>
+                <a href="Account.html">Sign Up </a> To View Our Full Menu!
+              </text>
+            </center>
+
+            <br></br>
+            <hr></hr>
+            <br></br>
+          </div>
+        </FadeIn>
       </div>
     );
   }
