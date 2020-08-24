6c06d91ee1b5fa89314a01b665439dd864377514
John Pwajok <johnpwajok97@hotmail.com>
2020-08-24 19:30:15 +0100

    second deploy attempt

--- server.js
+++ server.js
@@ -20,7 +20,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
-//Static file declarationapp.use(express.static(path.join(__dirname, 'client/build')));
-//production modeif(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));  //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
-//build modeapp.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
-//start serverapp.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
+//Static file declaration
+app.use(express.static(path.join(__dirname, "client/build")));
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build"))); //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+  //build mode
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  });
+  //start
+  serverapp.listen(port, (req, res) => {
+    console.log(`server listening on port: ${port}`);
+  });
+}
